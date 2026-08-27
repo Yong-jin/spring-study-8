c9ac6ce1a5f6f5a5f8292fde780bcc92bf4b7b7e
Yong-jin <takeardor@gmail.com>
2026-08-27 11:55:45 +0900

    파일처리 - 1 (파일처리 라이브러리 추가, 설정 추가, 파일 업로드 처리, 파일정보 DB 저장)

---
+++ SpringStudy8/src/main/java/com/app/dto/file/FileInfo.java
@@ -0,0 +1,19 @@
+package com.app.dto.file;
+
+import lombok.Data;
+
+@Data
+public class FileInfo {
+	
+	String fileName;	//실제 저장된 파일이름 (유니크한값) PK  39abo23osdfb09i.png
+	String originalFileName;  //사용자가 업로드 당시에 사용하던 원래 파일명 erd.png
+	String filePath;   // 파일이 저장된 경로
+	String urlFilePath; // 화면에 파일정보접근 표시할때, image url 경로로 접근할때
+	
+	//확장자
+	//파일사이즈
+	//업로드날짜
+	//콘텐츠타입
+	//...
+	
+}
